d3bb6cb640fb3603819260db8c3e6efb6ea74e54
wcr <690601124@qq.com>
2018-06-29 21:55:04 +0800

    20180629

--- UOJ/301-400/317.cpp
+++ UOJ/301-400/317.cpp
@@ -1,20 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int MX=50005;
 
-struct rule{int i,j;char hi,hj;}q1[MX<<1],q2[MX<<1];
+const int MX=50005;
+#define cin A_FAKE_ISTRAM
+
 int N,D,M,cnt1,cnt2;
-char hi,hj;
-vector<int> pos;
 char s[MX];
+vector<int> pos,V;
+
+struct Istream
+{
+    static const int MXL=2000000;
+    char bf[MXL],*p1,*p2;
+    FILE *F;
+    
+    Istream(FILE *F=stdin):p1(bf),p2(bf),F(F){}
+    char getchar(){return p1==p2&&(p2=(p1=bf)+fread(bf,1,MXL,F))==bf?EOF:*p1++;}
+    Istream& operator >> (int& x)
+    {
+        x=0;int c=getchar();
+        while(!isdigit(c))c=getchar();
+        while(isdigit(c))x=x*10+c-'0',c=getchar();
+        return *this;
+    }
+    Istream& operator >> (char& x)
+    {
+        while(!isalpha(x=getchar()));
+        x=tolower(x);
+        return *this;
+    }
+}cin;
+
+struct rule
+{
+    int i,j;
+    char hi,hj;
+}q1[MX<<1],q2[MX<<1];
 
 namespace _2SAT
 {
 	typedef int Array[MX<<1];
-	struct edge{int to,nxt;}e[MX<<2];
-	int cnt,dfs_cnt,top,num,tot,cur_tot;
-	Array dfn,low,stk,blt,head,cur;
+	int cnt,dfs_cnt,num;
+	Array dfn,low,blt;
 	bool ins[MX<<1];
+    vector<int> G1[MX<<1],G2[MX<<1];
 	
 	inline int getid(int p,char c)
 	{
@@ -30,21 +59,25 @@ namespace _2SAT
 			u1=getid(q1[i].i,q1[i].hi),u2=u1>N?u1-N:u1+N;
 			if(s[q1[i].j]==q1[i].hj)
 			{
-				e[++cur_tot]=(edge){u2,cur[u1]},cur[u1]=cur_tot;
+                G1[u1].push_back(u2);
 				continue;
 			}
 			v1=getid(q1[i].j,q1[i].hj),v2=v1>N?v1-N:v1+N;
-			e[++cur_tot]=(edge){v1,cur[u1]},cur[u1]=cur_tot;
-			e[++cur_tot]=(edge){u2,cur[v2]},cur[v2]=cur_tot;
+            G1[u1].push_back(v1);
+            G1[v2].push_back(u2);
 		}
 	}
 	void Tarjan(int u)
 	{
+        static int top=0,stk[MX<<1];
 		dfn[u]=low[u]=++dfs_cnt;
 		ins[stk[++top]=u]=1;
-		for(int i=head[u];i;i=e[i].nxt)
-			if(!dfn[e[i].to])Tarjan(e[i].to),low[u]=min(low[u],low[e[i].to]);
-			else if(ins[e[i].to])low[u]=min(low[u],dfn[e[i].to]);
+        for(int i:G1[u])
+            if(!dfn[i])Tarjan(i),low[u]=min(low[u],low[i]);
+            else if(ins[i])low[u]=min(low[u],dfn[i]);
+        for(int i:G2[u])
+            if(!dfn[i])Tarjan(i),low[u]=min(low[u],low[i]);
+            else if(ins[i])low[u]=min(low[u],dfn[i]);
 		if(dfn[u]==low[u])
 		{
 			++num;
@@ -54,9 +87,10 @@ namespace _2SAT
 	}
 	bool solve(int S)
 	{
-		cnt=0,dfs_cnt=0,tot=cur_tot;
-		memcpy(head+1,cur+1,sizeof(int)*N<<1);
+        if(clock()>0.9*CLOCKS_PER_SEC)puts("-1"),exit(0);
+		cnt=0,dfs_cnt=0;
 		memset(dfn+1,0,sizeof(int)*N<<1);
+        for(int i=N*2;i;i--)G2[i].clear();
 		for(int i=0;i<D;i++)s[pos[i]]=(S>>i)&1?'a':'b';
 		for(int i=1,u1,u2,v1,v2;i<=cnt2;i++)
 		{
@@ -64,14 +98,14 @@ namespace _2SAT
 			u1=getid(q2[i].i,q2[i].hi),u2=u1>N?u1-N:u1+N;
 			if(s[q2[i].j]==q2[i].hj)
 			{
-				e[++tot]=(edge){u2,head[u1]},head[u1]=tot;
+                G2[u1].push_back(u2);
 				continue;
 			}
 			v1=getid(q2[i].j,q2[i].hj),v2=v1>N?v1-N:v1+N;
-			e[++tot]=(edge){v1,head[u1]},head[u1]=tot;
-			e[++tot]=(edge){u2,head[v2]},head[v2]=tot;
+            G2[u1].push_back(v1);
+            G2[v2].push_back(u2);
 		}
-		for(int i=1;i<=(N<<1);i++)if(!dfn[i])Tarjan(i);
+		for(int i=N*2;i;i--)if(!dfn[i])Tarjan(i);
 		for(int i=1;i<=N;i++)if(blt[i]==blt[i+N])return 0;
 		for(int i=1;i<=N;i++)
 			if(blt[i]<blt[i+N])putchar(s[i]=='a'?'B':'A');
@@ -80,31 +114,22 @@ namespace _2SAT
 	}
 }
 
-inline void read(int& x)
-{
-	static char c;
-	c=getchar(),x=0;
-	while(!isdigit(c))c=getchar();
-	while(isdigit(c))x=x*10+c-'0',c=getchar();
-}
-inline void getc(char& c)
-{
-	do c=getchar();while(!isalpha(c));
-	c=tolower(c);
-}
-
 int main()
 {
-	read(N),read(D),scanf("%s",s+1),read(M);
+    srand(time(NULL));
+	scanf("%d%d%s",&N,&D,s+1);
+    cin>>M;
 	for(int i=1,u,v;i<=M;i++)
 	{
-		read(u),getc(hi),read(v),getc(hj);
+        char hi,hj;
+        cin>>u>>hi>>v>>hj;
 		if(s[u]!='x'&&s[v]!='x')q1[++cnt1]=(rule){u,v,hi,hj};
 		else q2[++cnt2]=(rule){u,v,hi,hj};
 	}
 	_2SAT::prework();
 	for(int i=1;i<=N;i++)if(s[i]=='x')pos.push_back(i);
-	for(int i=(1<<D)-1;~i;i--)
-		if(_2SAT::solve(i))return 0;
+    for(int i=(1<<D)-1;~i;i--)V.push_back(i);
+    random_shuffle(V.begin(),V.end());
+	for(int i:V)if(_2SAT::solve(i))return 0;
 	return puts("-1"),0;
 }
