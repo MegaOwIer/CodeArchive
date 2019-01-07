25ed4763d3eacf088a6bd57902b322519110f83b
wcr <690601124@qq.com>
2019-01-07 23:59:49 +0800

    20180330, Euler's Sieve, change indent from tab to space on 20190107

--- BZOJ/3001-4000/3994.cpp
+++ BZOJ/3001-4000/3994.cpp
@@ -8,39 +8,38 @@ LL ans;
 
 inline void read(int& x)
 {
-	static char c;
-	x=0,c=getchar();
-	while(!isdigit(c))c=getchar();
-	while(isdigit(c))x=x*10+c-'0',c=getchar();
+    static char c;
+    x=0,c=getchar();
+    while(!isdigit(c))c=getchar();
+    while(isdigit(c))x=x*10+c-'0',c=getchar();
 }
 
 int main()
 {
-	miu[1]=1;
-	for(int i=2,tmp;i<=50000;i++)
-	{
-		if(!d[i])pr[++cnt]=i,miu[i]=-1,d[i]=i;
-		for(int j=1;pr[j]<=d[i]&&(tmp=i*pr[j])<=50000;j++)
-		{
-			d[tmp]=pr[j],miu[tmp]=-miu[i];
-			if(d[i]==pr[j]){miu[tmp]=0;break;}
-		}
-	}
-	cerr<<cnt<<endl;
-	for(int i=2;i<=50000;i++)miu[i]+=miu[i-1];
-	for(int i=1;i<=50000;i++)for(int j=1,pos;j<=i;j=pos+1)
-		pos=i/(i/j),S[i]+=(LL)(i/j)*(pos-j+1);
-	read(T);
-	while(T--)
-	{
-		read(N),read(M),ans=0;
-		if(N>M)swap(N,M);
-		for(int i=1,pos;i<=N;i=pos+1)
-		{
-			pos=min(N/(N/i),M/(M/i));
-			ans+=(LL)(miu[pos]-miu[i-1])*S[N/i]*S[M/i];
-		}
-		printf("%lld\n",ans);
-	}
-	return 0;
+    miu[1]=1;
+    for(int i=2,tmp;i<=50000;i++)
+    {
+        if(!d[i])pr[++cnt]=i,miu[i]=-1,d[i]=i;
+        for(int j=1;pr[j]<=d[i]&&(tmp=i*pr[j])<=50000;j++)
+        {
+            d[tmp]=pr[j],miu[tmp]=-miu[i];
+            if(d[i]==pr[j]){miu[tmp]=0;break;}
+        }
+    }
+    for(int i=2;i<=50000;i++)miu[i]+=miu[i-1];
+    for(int i=1;i<=50000;i++)for(int j=1,pos;j<=i;j=pos+1)
+        pos=i/(i/j),S[i]+=(LL)(i/j)*(pos-j+1);
+    read(T);
+    while(T--)
+    {
+        read(N),read(M),ans=0;
+        if(N>M)swap(N,M);
+        for(int i=1,pos;i<=N;i=pos+1)
+        {
+            pos=min(N/(N/i),M/(M/i));
+            ans+=(LL)(miu[pos]-miu[i-1])*S[N/i]*S[M/i];
+        }
+        printf("%lld\n",ans);
+    }
+    return 0;
 }
